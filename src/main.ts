69e8d403f1b8bbed5fdc19e4b4537d87c5fdc0c1
Thomas Chapeaux <tc@saper-vedere.eu>
2023-11-03 14:56:05 +0100

    imp: enable Vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,15 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import dayjs from 'dayjs'
 import 'dayjs/locale/fr'
+import { inject } from '@vercel/analytics'
 
 import App from './App.vue'
 import router from './router'
 
 dayjs.locale('fr')
+inject() // Vercel analytics
 
 const app = createApp(App)
-
 app.use(router)
 
 app.mount('#app')
